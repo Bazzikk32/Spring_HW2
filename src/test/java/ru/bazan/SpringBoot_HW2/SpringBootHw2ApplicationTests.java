e0694f2c613e645da32180e339e6b755a9c65436
Bazzikk <bazanmikha@googlemail.com>
2024-11-17 00:32:01 +0300

    Web app as requested in task

---
+++ src/test/java/ru/bazan/SpringBoot_HW2/SpringBootHw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bazan.SpringBoot_HW2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
